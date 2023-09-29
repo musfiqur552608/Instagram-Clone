ede0ed67744d3c1eb7c33538242ef12f46fbb2a3
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-09-29 23:19:26 +0600

    profile and edit profile

--- app/src/main/java/org/freedu/instagramclone/SignUpActivity.kt
+++ app/src/main/java/org/freedu/instagramclone/SignUpActivity.kt
@@ -8,7 +8,9 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 import org.freedu.instagramclone.Models.User
 import org.freedu.instagramclone.databinding.ActivitySignUpBinding
 import org.freedu.instagramclone.utils.USER_NODE
@@ -20,43 +22,89 @@ class SignUpActivity : AppCompatActivity() {
         ActivitySignUpBinding.inflate(layoutInflater)
     }
     lateinit var user: User
-    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()){
-        uri->
+    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
         uri?.let {
-            uploadImage(uri, USER_PROFILE_FOLDER){
-                if(it==null){
+            uploadImage(uri, USER_PROFILE_FOLDER) {
+                if (it == null) {
 
-                }else{
+                } else {
                     user.image = it
                     binding.profileImage.setImageURI(uri)
                 }
             }
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         user = User()
+        if (intent.hasExtra("MODE")) {
+            if (intent.getIntExtra("MODE", -1) == 1) {
+                binding.signupBtn.text = "Update Profile"
+                Firebase.firestore.collection(USER_NODE).document(Firebase.auth.currentUser!!.uid)
+                    .get()
+                    .addOnSuccessListener {
+                        user = it.toObject<User>()!!
+                        if (!user.image.isNullOrBlank()) {
+                            Picasso.get().load(user.image).into(binding.profileImage)
+                        }
+                        binding.nameEtxt.editText?.setText(user.name)
+                        binding.emailEtxt.editText?.setText(user.email)
+                        binding.passEtxt.editText?.setText(user.password)
+
+                    }
+            }
+        }
         binding.signupBtn.setOnClickListener {
-            if((binding.nameEtxt.editText?.text.toString() == "") or (binding.emailEtxt.editText?.text.toString() == "") or (binding.passEtxt.editText?.text.toString() == "")){
-                Toast.makeText(this@SignUpActivity, "Please fill all the information", Toast.LENGTH_SHORT).show()
-            }else{
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
-                    binding.emailEtxt.editText?.text.toString(),
-                    binding.passEtxt.editText?.text.toString()
-                ).addOnCompleteListener {
-                    result ->
-                    if(result.isSuccessful){
-                        user.name = binding.nameEtxt.editText?.text.toString()
-                        user.email = binding.emailEtxt.editText?.text.toString()
-                        user.password = binding.passEtxt.editText?.text.toString()
-                        Firebase.firestore.collection(USER_NODE).document(Firebase.auth.currentUser!!.uid).set(user)
-                            .addOnSuccessListener {
-                                startActivity(Intent(this@SignUpActivity, HomeActivity::class.java))
-                                finish()
-                            }
-                    }else{
-                        Toast.makeText(this@SignUpActivity,result.exception?.localizedMessage, Toast.LENGTH_SHORT).show()
+            if (intent.hasExtra("MODE")) {
+                if (intent.getIntExtra("MODE", -1) == 1) {
+                    Firebase.firestore.collection(USER_NODE)
+                        .document(Firebase.auth.currentUser!!.uid).set(user)
+                        .addOnSuccessListener {
+                            startActivity(
+                                Intent(
+                                    this@SignUpActivity,
+                                    HomeActivity::class.java
+                                )
+                            )
+                            finish()
+                        }
+                }
+            } else {
+                if ((binding.nameEtxt.editText?.text.toString() == "") or (binding.emailEtxt.editText?.text.toString() == "") or (binding.passEtxt.editText?.text.toString() == "")) {
+                    Toast.makeText(
+                        this@SignUpActivity,
+                        "Please fill all the information",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                        binding.emailEtxt.editText?.text.toString(),
+                        binding.passEtxt.editText?.text.toString()
+                    ).addOnCompleteListener { result ->
+                        if (result.isSuccessful) {
+                            user.name = binding.nameEtxt.editText?.text.toString()
+                            user.email = binding.emailEtxt.editText?.text.toString()
+                            user.password = binding.passEtxt.editText?.text.toString()
+                            Firebase.firestore.collection(USER_NODE)
+                                .document(Firebase.auth.currentUser!!.uid).set(user)
+                                .addOnSuccessListener {
+                                    startActivity(
+                                        Intent(
+                                            this@SignUpActivity,
+                                            HomeActivity::class.java
+                                        )
+                                    )
+                                    finish()
+                                }
+                        } else {
+                            Toast.makeText(
+                                this@SignUpActivity,
+                                result.exception?.localizedMessage,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
                 }
             }
--- app/src/main/java/org/freedu/instagramclone/fragments/ProfileFragment.kt
+++ app/src/main/java/org/freedu/instagramclone/fragments/ProfileFragment.kt
@@ -1,32 +1,27 @@
 package org.freedu.instagramclone.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
+import org.freedu.instagramclone.Models.User
 import org.freedu.instagramclone.R
+import org.freedu.instagramclone.SignUpActivity
+import org.freedu.instagramclone.databinding.FragmentProfileBinding
+import org.freedu.instagramclone.utils.USER_NODE
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var binding:FragmentProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -34,26 +29,33 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.editProfileBtn.setOnClickListener {
+            val intent = Intent(activity, SignUpActivity::class.java)
+            intent.putExtra("MODE", 1)
+            activity?.startActivity(intent)
+            activity?.finish()
+        }
+
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Firebase.firestore.collection(USER_NODE).document(Firebase.auth.currentUser!!.uid).get()
+            .addOnSuccessListener {
+                val user: User = it.toObject<User>()!!
+                binding.nameTxt.text=user.name
+                binding.bioTxt.text=user.email
+
+                if(!user.image.isNullOrBlank()){
+                    Picasso.get().load(user.image).into(binding.profileImage)
                 }
             }
     }
