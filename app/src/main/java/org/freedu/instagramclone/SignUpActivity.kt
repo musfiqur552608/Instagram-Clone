231f0dd778bae56fceeeac04456c8f20b84865ea
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-09-29 19:51:23 +0600

    Signup and Iamge upload

--- app/src/main/java/org/freedu/instagramclone/SignUpActivity.kt
+++ app/src/main/java/org/freedu/instagramclone/SignUpActivity.kt
@@ -2,10 +2,66 @@ package org.freedu.instagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import org.freedu.instagramclone.Models.User
+import org.freedu.instagramclone.databinding.ActivitySignUpBinding
+import org.freedu.instagramclone.utils.USER_NODE
+import org.freedu.instagramclone.utils.USER_PROFILE_FOLDER
+import org.freedu.instagramclone.utils.uploadImage
 
 class SignUpActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
+    lateinit var user: User
+    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()){
+        uri->
+        uri?.let {
+            uploadImage(uri, USER_PROFILE_FOLDER){
+                if(it==null){
+
+                }else{
+                    user.image = it
+                    binding.profileImage.setImageURI(uri)
+                }
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+        user = User()
+        binding.signupBtn.setOnClickListener {
+            if((binding.nameEtxt.editText?.text.toString() == "") or (binding.emailEtxt.editText?.text.toString() == "") or (binding.passEtxt.editText?.text.toString() == "")){
+                Toast.makeText(this@SignUpActivity, "Please fill all the information", Toast.LENGTH_SHORT).show()
+            }else{
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.emailEtxt.editText?.text.toString(),
+                    binding.passEtxt.editText?.text.toString()
+                ).addOnCompleteListener {
+                    result ->
+                    if(result.isSuccessful){
+                        user.name = binding.nameEtxt.editText?.text.toString()
+                        user.email = binding.emailEtxt.editText?.text.toString()
+                        user.password = binding.passEtxt.editText?.text.toString()
+                        Firebase.firestore.collection(USER_NODE).document(Firebase.auth.currentUser!!.uid).set(user)
+                            .addOnSuccessListener {
+                                Toast.makeText(this@SignUpActivity,"User login", Toast.LENGTH_SHORT).show()
+                            }
+                    }else{
+                        Toast.makeText(this@SignUpActivity,result.exception?.localizedMessage, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+        binding.profileImage.setOnClickListener {
+            launcher.launch("image/*")
+        }
+
     }
 }
---
+++ app/src/main/java/org/freedu/instagramclone/utils/Constant.kt
@@ -0,0 +1,4 @@
+package org.freedu.instagramclone.utils
+
+const val USER_NODE = "User"
+const val USER_PROFILE_FOLDER = "Profile"
